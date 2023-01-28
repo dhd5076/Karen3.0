078720e43fd0d54fd6907aa36ec14bebe5589c7f
Dylan Dunn <dylandunn17@icloud.com>
2023-01-28 13:39:33 -0500

    Switched to Next.Js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGO_URL: "mongodb://ddunn:Robod123!@karen/karen"
+  }
 }
 
 module.exports = nextConfig
